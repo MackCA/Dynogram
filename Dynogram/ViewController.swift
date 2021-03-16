643efa4b425f9db964fc0d4fa9607709e210e1d5
Chloe Mack <chloe.mack@stonybrook.edu>
2021-03-16 02:37:02 -0400

    Initial Commit

---
+++ Dynogram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dynogram
+//
+//  Created by Chloe Mack on 3/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
