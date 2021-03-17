a0314032afe6b3ec5ecc81749e4b2669b56ee5c0
Chloe Mack <chloe.mack@stonybrook.edu>
2021-03-17 03:06:29 -0400

    Initial Commit

--- Dynogram/AppDelegate.swift
+++ Dynogram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "IKdAmgHk69S8hfDZ9A9lC93NnFlLfcXGpfwb4shQ"
+                    $0.clientKey = "CHZcPNrLXXs8KWmZXNwIxGTR0IyanZzoN4tjMh8q"
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 
